79b3a7a6c24003502e3723dfd4e66b1f0b8fdcf4
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-12 17:28:34 +1200

    default converter values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,18 +1,14 @@
 # ![](./assets/logo.png)  
 
-## Save 1.0.0-beta.7 for in-20100110  
+## Save 1.0.0-rc.1 for in-20100110  
 
-### Changes  
-#### Technical Changes  
-- Includes [ClassicExplorer](https://github.com/bluecrab2/ClassicExplorer) v1.1.  
-  - Used with permission from bluecrab2.  
+### Changes
+#### Config  
+- Added default converter values.  
+  - These can only be changed by editing the config file.  
 #### Converter  
-- Successful convert screen now shows where the level was saved.  
-- Classic:v2 worlds can now be converted.  
-  - Note: More work is to be done to allow conversion of inventory, etc.  
-#### GUI  
-- Added a **Credits and Attributions** screen.
-  - This screen can be accessed via the **Config** screen, and scrolls through credits and attributions before returning you to the config screen.   
+- All classic saves will now use ClassicExplorer to convert.  
+- Conversion will fail if the blocks can't be found or if they don't match the output of `height*length*width`.
 
 #  
 Licensed under LGPL-3.0-or-later.  
--- src/main/java/com/mclegoman/mclm_save/config/SaveConfig.java
+++ src/main/java/com/mclegoman/mclm_save/config/SaveConfig.java
@@ -14,6 +14,8 @@ import org.quiltmc.config.api.values.TrackedValue;
 import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.loader.api.config.v2.QuiltConfig;
 
+import java.time.Instant;
+
 public class SaveConfig extends ReflectiveConfig {
 	public static final SaveConfig instance = QuiltConfig.create(Data.version.getID(), Data.version.getID(), SaveConfig.class);
 	@Comment("When set to true, the Save and Load level buttons will open the dialog directly.")
@@ -26,4 +28,38 @@ public class SaveConfig extends ReflectiveConfig {
 	public final TrackedValue<Theme> dialogTheme = this.value(Theme.system);
 	@Comment("This sets where the save/load dialog opens")
 	public final TrackedValue<String> dialogDir = this.value(QuiltLoader.getGameDir().toString());
+	@Comment("This sets the default name of a classic world if it can't be found in the save.")
+	public final TrackedValue<String> convertClassicDefaultName = this.value("A Nice World");
+	@Comment("This sets the default creator of a classic world if it can't be found in the save.")
+	public final TrackedValue<String> convertClassicDefaultCreator = this.value("Player");
+	@Comment("This sets the default cloud color of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultCloudColor = this.value(0xFFFFFF);
+	@Comment("This sets the default cloud height of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultCloudHeight = this.value(66);
+	@Comment("This sets the default fog color of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultFogColor = this.value(0xFFFFFF);
+	@Comment("This sets the default sky brightness of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSkyBrightness = this.value(100);
+	@Comment("This sets the default sky color of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSkyColor = this.value(10079487);
+	@Comment("This sets the default surrounding ground height of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSurroundingGroundHeight = this.value(23);
+	@Comment("This sets the default surrounding ground type of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSurroundingGroundType = this.value(2);
+	@Comment("This sets the default surrounding water height of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSurroundingWaterHeight = this.value(32);
+	@Comment("This sets the default surrounding water type of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSurroundingWaterType = this.value(8);
+	@Comment("This sets the default spawnX of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSpawnX = this.value(128);
+	@Comment("This sets the default spawnY of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSpawnY = this.value(36);
+	@Comment("This sets the default spawnZ of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultSpawnZ = this.value(128);
+	@Comment("This sets the default height of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultHeight = this.value(64);
+	@Comment("This sets the default length of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultLength = this.value(256);
+	@Comment("This sets the default width of a classic world if it can't be found in the save.")
+	public final TrackedValue<Integer> convertClassicDefaultWidth = this.value(256);
 }
