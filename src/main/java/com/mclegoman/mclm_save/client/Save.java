c162cf2a87e5b9534a666d6de05ddab4cb607bc9
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-18 12:09:27 +1200

    rc.2 - the death screen will now automatically close if the player has more than 0 health.

--- src/main/java/com/mclegoman/mclm_save/client/Save.java
+++ src/main/java/com/mclegoman/mclm_save/client/Save.java
@@ -8,8 +8,11 @@
 package com.mclegoman.mclm_save.client;
 
 import com.mclegoman.mclm_save.client.april_fools.AprilFools;
+import com.mclegoman.mclm_save.client.data.ClientData;
 import com.mclegoman.mclm_save.client.level.LevelFile;
+import com.mclegoman.mclm_save.common.data.Data;
 import com.mclegoman.mclm_save.common.util.Couple;
+import net.minecraft.client.gui.screen.DeathScreen;
 import org.quiltmc.loader.api.ModContainer;
 
 public class Save {
@@ -26,6 +29,12 @@ public class Save {
 			if ((boolean) loadData.getFirst()) {
 				LevelFile.shouldLoad = new Couple(false, true);
 				LevelFile.loadWorld((boolean)loadData.getSecond());
+			}
+		}
+		if (ClientData.minecraft.f_0723335 instanceof DeathScreen) {
+			if (ClientData.minecraft.f_6058446.health > 0) {
+				ClientData.minecraft.f_6058446.deathTime = 0;
+				ClientData.minecraft.m_6408915(null);
 			}
 		}
 	}
