fd0194a421e52c38881ae788371829da0209c3bd
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-22 14:35:34 +1200

    updated saveWorldOnExit default to false. (unless we have any major changes, this will just be in the release.1)

--- src/main/java/com/mclegoman/mclm_save/config/SaveConfig.java
+++ src/main/java/com/mclegoman/mclm_save/config/SaveConfig.java
@@ -65,5 +65,5 @@ public class SaveConfig extends ReflectiveConfig {
 	@Comment("This sets the default width of a classic world if it can't be found in the save.")
 	public final TrackedValue<Integer> convertClassicDefaultWidth = this.value(256);
 	@Comment("When set to true, the current world will be saved to %rundir%/level.mclevel")
-	public final TrackedValue<Boolean> saveWorldOnExit = this.value(true);
+	public final TrackedValue<Boolean> saveWorldOnExit = this.value(false);
 }
