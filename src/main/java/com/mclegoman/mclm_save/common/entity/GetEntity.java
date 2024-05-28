3d49a0057915d2f8ad1487b67534c7c129dd5c48
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-28 15:36:30 +1200

    only move living entities out of blocks, and if the coords are within the world check those instead of defaulting.

---
+++ src/main/java/com/mclegoman/mclm_save/common/entity/GetEntity.java
@@ -0,0 +1,14 @@
+/*
+    mclm_save
+    Contributor(s): MCLegoMan
+    Github: https://github.com/MCLegoMan/mclm_save
+    Licence: GNU LGPLv3
+*/
+
+package com.mclegoman.mclm_save.common.entity;
+
+import net.minecraft.entity.Entity;
+
+public interface GetEntity {
+	Entity save$getEntity();
+}
