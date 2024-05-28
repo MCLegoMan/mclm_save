34bb488bae0c51c658f2c015468dded19400c3cd
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-28 16:02:22 +1200

    104 Block Saving Converter.

--- src/main/java/com/mclegoman/mclm_save/client/level/Level.java
+++ src/main/java/com/mclegoman/mclm_save/client/level/Level.java
@@ -89,7 +89,7 @@ public abstract class Level {
 
 	public final void save(World world, File file) throws IOException {
 		FileOutputStream outputStream = new FileOutputStream(file);
-		TagCompound level = LevelFile.createLevel("Player", Instant.now().getEpochSecond(), "A Nice World", world.f_1709243, (short) world.f_4971921, world.f_2946178, (byte) (world.f_6732352 * 100.0F), (int) world.f_3766825, (short) world.f_0183464, (byte) Block.GRASS.id, (short) world.f_8873427, (byte) world.f_3241378, (short) world.f_3926541, (short) world.f_2923303, (short) world.f_8500813, (short) world.f_4184003, (short) world.f_8212213, (short) world.f_3061106, Accessors.World.f_4249554);
+		TagCompound level = LevelFile.createLevel("Player", Instant.now().getEpochSecond(), "A Nice World", world.f_1709243, (short) world.f_4971921, world.f_2946178, (byte) (world.f_6732352 * 100.0F), world.f_3766825, (short) world.f_0183464, (byte) Block.GRASS.id, (short) world.f_8873427, (byte) world.f_3241378, (short) world.f_3926541, (short) world.f_2923303, (short) world.f_8500813, (short) world.f_4184003, (short) world.f_8212213, (short) world.f_3061106, Accessors.World.f_4249554);
 		TagList entities = new TagList();
 		for (Object entity : world.f_7148360.f_6899876) {
 			Entity currentEntity = (Entity)entity;
@@ -138,6 +138,9 @@ public abstract class Level {
 				int count = index.getByte("Count");
 				int id = index.getShort("id");
 				int slot = index.getByte("Slot");
+				int blockId = index.getByte("blockId");
+				// 104 Block Saving Converter.
+				if (id == -1 && blockId != 0 && Block.BY_ID[blockId] != null) id = blockId;
 				setStack(playerInventory.inventorySlots, slot, new ItemStack(id, count));
 			}
 			((PlayerEntity)entity).inventory = playerInventory;
