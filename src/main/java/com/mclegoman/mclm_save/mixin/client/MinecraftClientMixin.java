bc66e20da27d8ece6ce54df83ef301c325e5e147
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-07-12 04:32:00 +1200

    accessors

--- src/main/java/com/mclegoman/mclm_save/mixin/client/MinecraftClientMixin.java
+++ src/main/java/com/mclegoman/mclm_save/mixin/client/MinecraftClientMixin.java
@@ -11,6 +11,7 @@ import com.mclegoman.mclm_save.client.Save;
 import com.mclegoman.mclm_save.client.util.Accessors;
 import com.mclegoman.mclm_save.common.data.Data;
 import net.minecraft.client.C_5664496;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.world.World;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.loader.api.QuiltLoader;
@@ -27,10 +28,12 @@ import java.util.Optional;
 public abstract class MinecraftClientMixin {
 	@Shadow private Canvas f_0769488;
 	@Shadow protected abstract void m_8603410(World world);
+	@Shadow private Screen f_0723335;
 	@Inject(method = "m_8832598", at = @At(value = "TAIL"))
 	private void save$tick(CallbackInfo ci) {
 		// Updates Canvas Accessor and checks if the world needs to load.
 		Accessors.MinecraftClient.canvas = this.f_0769488;
+		Accessors.MinecraftClient.screen = this.f_0723335;
 		if (Accessors.MinecraftClient.shouldLoad && Accessors.MinecraftClient.world != null) {
 			Accessors.MinecraftClient.shouldLoad = false;
 			this.m_8603410(Accessors.MinecraftClient.world);
